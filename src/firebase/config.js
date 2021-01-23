4ce60b9014bb5959fd504daa903d69ea5b822d13
Amit Sakharkar <amit@smilebots.com>
2021-01-23 11:13:31 +0530

    basic setup done and firebase hosted.

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyDxAS0Ro6CErxFE71CgzMNbhtvS_MQrMEg",
+  authDomain: "a-music-63261.firebaseapp.com",
+  projectId: "a-music-63261",
+  storageBucket: "a-music-63261.appspot.com",
+  messagingSenderId: "820681718891",
+  appId: "1:820681718891:web:ef94df70085a2a8772852e",
+  measurementId: "G-D5NEZ36GC7",
+};
+export default config;
